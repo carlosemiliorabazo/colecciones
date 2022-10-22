f8e045bea16c1e0be77e6e803ff11a1052e3f991
Carlos Rabazo <carlosemilio.rabazo@ufv.es>
2022-10-22 17:22:09 +0200

    Octavo commit introducimos ajustes en los comentarios

--- src/com/ufv/dis/colecciones/MiColeccion.java
+++ src/com/ufv/dis/colecciones/MiColeccion.java
@@ -1,10 +1,8 @@
 package com.ufv.dis.colecciones;
-
 public class MiColeccion {
     private Object datos[] = null ;
     private int len = 0;
     // En el constructor se especifica la cantidad inicial
-
     public MiColeccion(int capacidadInicial){
         datos = new Object[capacidadInicial];
     }
@@ -18,15 +16,18 @@ public class MiColeccion {
     public int cantidad(){
         return len;
     }
-
+    // inserta un elemento en la i-ésima posición del array
     public void insertar(Object elm, int i){
+        // capacidad del array colmada?
         if (len==datos.length){
+            // creamos nuevo array con el doble capacidad del anterior
             Object aux[] = datos;
             datos = new Object[datos.length*2];
+            // copiamos en el array los elementos del anterior
             for (int j=0; j<len; j++){
               datos[j]=aux[j];
             }
-            aux=null;
+            aux=null; // asignamos null al array anterior
         }
         for (int j=len-1; j>=i; j--){
             datos[j+1]=datos[j];
@@ -34,7 +35,7 @@ public class MiColeccion {
         datos[i]=elm;
         len++;
     }
-
+    // recorre el array mientras no encuentre el elemento que busca
     public int buscar(Object elm){
         int i=0;
         // mientras no me pase del tope y mientras no encuentre...
@@ -42,17 +43,18 @@ public class MiColeccion {
         // si no me pasé entonces encontré, si no, no encontré
         return i<len ? i : -1;
     }
-
+    // agrega un elemento al final del array
     public void agregar(Object elm){
         insertar(elm, len);
     }
-    // elimina un elemento desplazando los demas hacia arriba
+    // elimina el i-ésimo elemento desplazando hacia arriba
+    // los elementos que están a partir de la posición i+1
     public Object eliminar(int i){
         Object aux = datos[i];
         for (int j=i; j<len-1; j++){
             datos[j]=datos[j+1];
         }
         len--;
-        return aux;
+        return aux; // retorna el elemento eliminado de la colección
     }
 }
--- src/com/ufv/dis/colecciones/TestMiColeccion.java
+++ src/com/ufv/dis/colecciones/TestMiColeccion.java
@@ -1,15 +1,12 @@
 package com.ufv.dis.colecciones;
-// Muestra una lista de nombres en orden inverso
 import java.util.Scanner;
-public class TestMiColeccion {
+public class TestMiColeccion { // Muestra una lista de nombres en orden inverso
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         // Creo una colección de capacidad inicial 5
         MiColeccion mc = new MiColeccion(5);
-        // Leo el primer nombre
         System.out.println("Ingrese nombre: ");
-        String nom=scanner.next();
-
+        String nom=scanner.next(); // Leo el primer nombre
         while (!nom.equals("FIN")){
             // inserto siempre en la posicion 0
             mc.insertar(nom,0);
@@ -17,11 +14,9 @@ public class TestMiColeccion {
             nom=scanner.next();
         }
         String aux;
-
         // recorro la colección y tomo cada uno de sus elementos
         for (int i=0; i<mc.cantidad(); i++){
-            // el metodo obtener retorna un Object entonces
-            // tengo que castear a String
+            // el metodo obtener retorna un Object que tengo que castear a String
             aux = (String)mc.obtener(i);
             System.out.println(aux + " - " + aux.length() + " caracteres");
         }
